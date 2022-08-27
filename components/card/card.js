576998907c6753e3f6b6c685d67461b27a85852c
= <=>
2022-08-27 17:29:47 +0530

    added error image

--- components/card/card.js
+++ components/card/card.js
@@ -5,7 +5,10 @@ import Image from "next/image";
 import styles from "./card.module.css";
 
 const Card = (props) => {
-  const { imgUrl = "/static/clifforsd.webp", size = "medium" } = props;
+  const {
+    imgUrl = "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80",
+    size = "medium",
+  } = props;
 
   const [imgSrc, setImgSrc] = useState(imgUrl);
 
@@ -17,7 +20,9 @@ const Card = (props) => {
 
   const handleOnError = () => {
     console.log("Hi Error");
-    setImgSrc("/static/clifford.webp");
+    setImgSrc(
+      "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1159&q=80"
+    );
   };
 
   return (
