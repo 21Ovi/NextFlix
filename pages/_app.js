31205c3c17cbc1b90238a12476aebec60a0bfc6c
= <=>
2022-08-31 16:01:49 +0530

    restricted routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { magic } from "../lib/magic-client";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const router = useRouter();
+
+  useEffect(async () => {
+    const isLoggedIn = await magic.user.isLoggedIn();
+    if (isLoggedIn) {
+      router.push("/");
+    } else {
+      router.push("/login");
+    }
+  }, []);
+
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
