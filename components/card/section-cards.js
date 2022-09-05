4f0f343fb7d11efb584b84876bb7b45a75384b85
= <=>
2022-09-06 00:09:37 +0530

    modal creted

--- components/card/section-cards.js
+++ components/card/section-cards.js
@@ -12,7 +12,6 @@ const SectionCards = (props) => {
       <h2 className={styles.title}>{title}</h2>
       <div className={styles.cardWrapper}>
         {videos.map((video, idx) => {
-          console.log({ video });
           return (
             <Link href={`/video/${video.id}`}>
               <Card id={idx} imgUrl={video.imgUrl} size={size} />
