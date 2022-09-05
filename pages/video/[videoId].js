cd7e36a0252309a77e3f773fac67f68f3c2404a3
= <=>
2022-09-06 00:28:21 +0530

    video Player added

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -18,7 +18,16 @@ const Video = () => {
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <div>Modal Body</div>
+        <div>
+          <iframe
+            id="ytplayer"
+            type="text/html"
+            width="640"
+            height="360"
+            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
+            frameborder="0"
+          ></iframe>
+        </div>
       </Modal>
     </div>
   );
