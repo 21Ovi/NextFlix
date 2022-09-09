f910822760fe1dba3cd66df58aa3e006f195c55e
= <=>
2022-09-10 01:54:47 +0530

    didtoken from magic to server updated

--- components/nav/navbar.js
+++ components/nav/navbar.js
@@ -31,7 +31,9 @@ const NavBar = () => {
   useEffect(() => {
     async function getUsername() {
       try {
-        const { email } = await magic.user.getMetadata();
+        const { email, issuer } = await magic.user.getMetadata();
+        const didToken = await magic.user.getIdToken();
+        console.log({ didToken });
         if (email) {
           setUsername(email);
         }
--- pages/api/login.js
+++ pages/api/login.js
@@ -1,13 +1,19 @@
+import { magicAdmin } from "../../lib/magic";
+
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
       const auth = req.headers.authorization;
-      const token = auth ? auth.substr(7) : "";
-      console.log(token);
+      const didToken = auth ? auth.substr(7) : "";
+      console.log({ didToken });
+
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+      console.log({ metadata });
+
+      //invoke magic
       res.send({ done: true });
-      // invoke magic
-    } catch (err) {
-      console.error("Something went wrong logging in");
+    } catch (error) {
+      console.error("Something went wrong logging in", error);
       res.status(500).send({ done: false });
     }
   } else {
