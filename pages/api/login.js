77f96480c58d8a60bf2ad837e8073c893dd599ba
= <=>
2022-09-10 02:19:01 +0530

    didtoken from magic to server updated

--- pages/api/login.js
+++ pages/api/login.js
@@ -1,4 +1,5 @@
 import { magicAdmin } from "../../lib/magic";
+import jwt from "jsonwebtoken";
 
 export default async function login(req, res) {
   if (req.method === "POST") {
@@ -10,7 +11,23 @@ export default async function login(req, res) {
       const metadata = await magicAdmin.users.getMetadataByToken(didToken);
       console.log({ metadata });
 
-      //invoke magic
+      //create jwt
+
+      const token = jwt.sign(
+        {
+          ...metadata,
+          iat: Math.floor(Date.now() / 1000),
+          exp: Math.floor(Date.now() / 1000 + 7 * 24 * 60 * 60),
+          "https://hasura.io/jwt/claims": {
+            "x-hasura-allowed-roles": ["user", "admin"],
+            "x-hasura-default-role": "user",
+            "x-hasura-user-id": `${metadata.issuer}`,
+          },
+        },
+        "ThisIsASecret123321TokenSecret21"
+      );
+      console.log({ token });
+
       res.send({ done: true });
     } catch (error) {
       console.error("Something went wrong logging in", error);
