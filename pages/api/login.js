8f376b2f87aa9bc414f11ff8c02437c8bab7b438
= <=>
2022-09-17 03:25:04 +0530

    meta data extravcted

--- pages/api/login.js
+++ pages/api/login.js
@@ -39,9 +39,19 @@
 //   }
 // }
 
+import { magicAdmin } from "../../lib/magic";
+
 export default async function login(req, res) {
   if (req.method === "POST") {
     try {
+      const auth = req.headers.authorization;
+      const didToken = auth ? auth.substr(7) : "";
+      console.log({ didToken });
+
+      const metadata = await magicAdmin.users.getMetadataByToken(didToken);
+      console.log({ metadata });
+
+      //invoke magic
       res.send({ done: true });
     } catch (err) {
       console.error("Something went wrong logging in", err);
