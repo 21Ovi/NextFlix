46bf0fed59c53576ae711914d2b5582c3e8ccf23
= <=>
2022-09-20 00:35:01 +0530

    rating system added

--- pages/video/[videoId].js
+++ pages/video/[videoId].js
@@ -1,13 +1,12 @@
-/* eslint-disable react/no-unknown-property */
 import { useState } from "react";
 import { useRouter } from "next/router";
 import Modal from "react-modal";
 import styles from "../../styles/Video.module.css";
 
+import NavBar from "../../components/nav/navbar";
 import clsx from "classnames";
 
 import { getYoutubeVideoById } from "../../lib/videos";
-import NavBar from "../../components/nav/navbar";
 
 import Like from "../../components/icons/like-icon";
 import DisLike from "../../components/icons/dislike-icon";
@@ -16,19 +15,19 @@ Modal.setAppElement("#__next");
 
 export async function getStaticProps(context) {
   const videoId = context.params.videoId;
+
   const videoArray = await getYoutubeVideoById(videoId);
 
   return {
     props: {
       video: videoArray.length > 0 ? videoArray[0] : {},
     },
-    revalidate: 10,
+    revalidate: 10, // In seconds
   };
 }
 
 export async function getStaticPaths() {
   const listOfVideos = ["mYfJxlgR2jw", "4zH5iYM4wJo", "KCPEHsAViiQ"];
-
   const paths = listOfVideos.map((videoId) => ({
     params: { videoId },
   }));
@@ -37,6 +36,12 @@ export async function getStaticPaths() {
 }
 
 const Video = ({ video }) => {
+  const router = useRouter();
+  const videoId = router.query.videoId;
+
+  const [toggleLike, setToggleLike] = useState(false);
+  const [toggleDisLike, setToggleDisLike] = useState(false);
+
   const {
     title,
     publishTime,
@@ -45,74 +50,92 @@ const Video = ({ video }) => {
     statistics: { viewCount } = { viewCount: 0 },
   } = video;
 
-  const [toggleLike, setToggleLike] = useState(false);
-  const [toggleDislike, setToggleDislike] = useState(false);
-
-  const handleToggleDislike = () => {
-    setToggleDislike(!toggleDislike);
-    setToggleLike(toggleDislike);
+  const runRatingService = async (favourited) => {
+    const response = await fetch("/api/stats", {
+      method: "POST",
+      body: JSON.stringify({
+        videoId,
+        favourited,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log("data", await response.json());
   };
 
-  const handleToggleLike = () => {
+  const handleToggleDislike = async () => {
+    console.log("handleToggleDislike");
+    setToggleDisLike(!toggleDisLike);
+    setToggleLike(toggleDisLike);
+
+    const val = !toggleDisLike;
+    const favourited = val ? 0 : 1;
+    const response = await runRatingService(favourited);
+    console.log("data", await response.json());
+  };
+
+  const handleToggleLike = async () => {
+    console.log("handleToggleLike");
+    const val = !toggleLike;
     setToggleLike(!toggleLike);
-    setToggleDislike(toggleLike);
+    setToggleDisLike(toggleLike);
+
+    const favourited = val ? 1 : 0;
+    const response = await runRatingService(favourited);
+    console.log("data", await response.json());
   };
 
-  const router = useRouter();
   return (
     <div className={styles.container}>
       <NavBar />
       <Modal
         isOpen={true}
-        contentLabel="Watch the Video"
-        onRequestClose={() => {
-          router.back();
-        }}
+        contentLabel="Watch the video"
+        onRequestClose={() => router.back()}
         className={styles.modal}
         overlayClassName={styles.overlay}
       >
-        <div>
-          <iframe
-            id="ytplayer"
-            className={styles.videoPlayer}
-            type="text/html"
-            width="100%"
-            height="360"
-            src={`https://www.youtube.com/embed/${router.query.videoId}?autoplay=0&origin=http://example.com&controls=0&rel=0`}
-            frameborder="0"
-          ></iframe>
-          <div className={styles.likeDislikeBtnWrapper}>
-            <div className={styles.likeBtnWrapper}>
-              <button onClick={handleToggleLike}>
-                <div className={styles.btnWrapper}>
-                  <Like selected={toggleLike} />
-                </div>
-              </button>
-            </div>
+        <iframe
+          id="ytplayer"
+          className={styles.videoPlayer}
+          type="text/html"
+          width="100%"
+          height="360"
+          src={`https://www.youtube.com/embed/${videoId}?autoplay=0&origin=http://example.com&controls=0&rel=1`}
+          frameBorder="0"
+        ></iframe>
 
-            <button onClick={handleToggleDislike}>
+        <div className={styles.likeDislikeBtnWrapper}>
+          <div className={styles.likeBtnWrapper}>
+            <button onClick={handleToggleLike}>
               <div className={styles.btnWrapper}>
-                <DisLike selected={toggleDislike} />
+                <Like selected={toggleLike} />
               </div>
             </button>
           </div>
-          <div className={styles.modalBody}>
-            <div className={styles.modalBodyContent}>
-              <div className={styles.col1}>
-                <p className={styles.publishTime}>{publishTime}</p>
-                <p className={styles.title}>{title}</p>
-                <p className={styles.description}>{description}</p>
-              </div>
-              <div className={styles.col2}>
-                <p className={clsx(styles.subText, styles.subTextWrapper)}>
-                  <span className={styles.textColor}>Cast : </span>
-                  <span className={styles.channelTitle}>{channelTitle}</span>
-                </p>
-                <p className={clsx(styles.subText, styles.subTextWrapper)}>
-                  <span className={styles.textColor}>View Count : </span>
-                  <span className={styles.channelTitle}>{viewCount}</span>
-                </p>
-              </div>
+          <button onClick={handleToggleDislike}>
+            <div className={styles.btnWrapper}>
+              <DisLike selected={toggleDisLike} />
+            </div>
+          </button>
+        </div>
+        <div className={styles.modalBody}>
+          <div className={styles.modalBodyContent}>
+            <div className={styles.col1}>
+              <p className={styles.publishTime}>{publishTime}</p>
+              <p className={styles.title}>{title}</p>
+              <p className={styles.description}>{description}</p>
+            </div>
+            <div className={styles.col2}>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>Cast: </span>
+                <span className={styles.channelTitle}>{channelTitle}</span>
+              </p>
+              <p className={clsx(styles.subText, styles.subTextWrapper)}>
+                <span className={styles.textColor}>View Count: </span>
+                <span className={styles.channelTitle}>{viewCount}</span>
+              </p>
             </div>
           </div>
         </div>
