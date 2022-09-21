09f8bd161d6bf7b5641367c1750b8c3710b64b03
= <=>
2022-09-21 12:56:48 +0530

    stats APi tweaked

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -17,11 +17,8 @@ export default async function stats(req, resp) {
         if (videoId) {
           const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
           const userId = decodedToken.issuer;
-          const doesStatsExist = await findVideoIdByUser(
-            token,
-            userId,
-            videoId
-          );
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
           if (doesStatsExist) {
             const response = await updateStats(token, {
               watched,
@@ -38,6 +35,30 @@ export default async function stats(req, resp) {
               favourited,
             });
             resp.send({ data: response });
+          }
+        }
+      }
+    } catch (error) {
+      console.error("Error occoured /stats", error);
+      resp.status(500).send({ done: false, error: error?.message });
+    }
+  } else {
+    try {
+      const token = req.cookies.token;
+      if (!token) {
+        resp.status(403).send({});
+      } else {
+        const { videoId, favourited, watched = true } = req.body;
+        if (videoId) {
+          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+          const userId = decodedToken.issuer;
+          const findVideo = await findVideoIdByUser(token, userId, videoId);
+          const doesStatsExist = findVideo?.length > 0;
+          if (doesStatsExist) {
+            resp.send(findVideo);
+          } else {
+            resp.status(404);
+            resp.send({ user: null, msg: "Video not found" });
           }
         }
       }
