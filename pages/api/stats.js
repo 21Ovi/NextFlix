2cd8b52c5759405f42d4ea54b71ce65a86f30256
= <=>
2022-09-21 13:10:30 +0530

    refactored stats API

--- pages/api/stats.js
+++ pages/api/stats.js
@@ -1,25 +1,29 @@
 import jwt from "jsonwebtoken";
-
 import {
   findVideoIdByUser,
-  insertStats,
   updateStats,
+  insertStats,
 } from "../../lib/db/hasura";
 
 export default async function stats(req, resp) {
-  if (req.method === "POST") {
-    try {
-      const token = req.cookies.token;
-      if (!token) {
-        resp.status(403).send({});
-      } else {
-        const { videoId, favourited, watched = true } = req.body;
-        if (videoId) {
-          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-          const userId = decodedToken.issuer;
-          const findVideo = await findVideoIdByUser(token, userId, videoId);
-          const doesStatsExist = findVideo?.length > 0;
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      resp.status(403).send({});
+    } else {
+      const inputParams = req.method === "POST" ? req.body : req.query;
+      const { videoId } = inputParams;
+      if (videoId) {
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+        const userId = decodedToken.issuer;
+        const findVideo = await findVideoIdByUser(token, userId, videoId);
+        const doesStatsExist = findVideo?.length > 0;
+
+        if (req.method === "POST") {
+          const { favourited, watched = true } = req.body;
           if (doesStatsExist) {
+            // update it
             const response = await updateStats(token, {
               watched,
               userId,
@@ -28,6 +32,8 @@ export default async function stats(req, resp) {
             });
             resp.send({ data: response });
           } else {
+            // add it
+            console.log({ watched, userId, videoId, favourited });
             const response = await insertStats(token, {
               watched,
               userId,
@@ -36,24 +42,7 @@ export default async function stats(req, resp) {
             });
             resp.send({ data: response });
           }
-        }
-      }
-    } catch (error) {
-      console.error("Error occoured /stats", error);
-      resp.status(500).send({ done: false, error: error?.message });
-    }
-  } else {
-    try {
-      const token = req.cookies.token;
-      if (!token) {
-        resp.status(403).send({});
-      } else {
-        const { videoId, favourited, watched = true } = req.body;
-        if (videoId) {
-          const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-          const userId = decodedToken.issuer;
-          const findVideo = await findVideoIdByUser(token, userId, videoId);
-          const doesStatsExist = findVideo?.length > 0;
+        } else {
           if (doesStatsExist) {
             resp.send(findVideo);
           } else {
@@ -62,9 +51,9 @@ export default async function stats(req, resp) {
           }
         }
       }
-    } catch (error) {
-      console.error("Error occoured /stats", error);
-      resp.status(500).send({ done: false, error: error?.message });
     }
+  } catch (error) {
+    console.error("Error occurred /stats", error);
+    resp.status(500).send({ done: false, error: error?.message });
   }
 }
