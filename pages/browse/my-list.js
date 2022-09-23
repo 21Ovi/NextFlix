7fe0d64de8ca2f998cb2a68ecd30cb58ef3ac14e
= <=>
2022-09-24 00:21:07 +0530

    myListStyles

--- pages/browse/my-list.js
+++ pages/browse/my-list.js
@@ -2,15 +2,17 @@ import Head from "next/head";
 import NavBar from "../../components/nav/navbar";
 import SectionCard from "../../components/card/section-cards";
 
+import styles from "../../styles/MyList.module.css";
+
 const MyList = () => {
   return (
     <div>
       <Head>
         <title>My List</title>
       </Head>
-      <main>
+      <main className={styles.main}>
         <NavBar />
-        <div>
+        <div className={styles.sectionWrapper}>
           <SectionCard title="My List" videos={[]} size="small" />
         </div>
       </main>
